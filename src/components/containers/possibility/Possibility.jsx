67a35878ea763d159ea1b626302e0fecb8254a74
Vols40 <150765619+Vols40@users.noreply.github.com>
2023-11-26 22:55:13 +0200

    am lucrat un pic la aplicatie

---
+++ src/components/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './posssibility.css';
+
+const Possibility = () => {
+  return (
+    <div>
+      Possibility
+    </div>
+  )
+}
+
+export default Possibility
---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './feature.css';
+
+const feature = () => {
+  return (
+    <div>
+      Featurew
+    </div>
+  )
+}
+
+export default Feature
